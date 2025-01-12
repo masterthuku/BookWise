1a1b4bb886170f1db461344ab67e75776b17fa75
masterthuku <peterthuku706@gmail.com>
2025-01-12 10:07:39 +0300

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React from 'react'
+
+const Header = () => {
+    const pathName = usePathname();
+  return (
+    <header className='my-10 flex justify-between gap-5'>
+        <Link href={"/"}>
+        <Image src="/icons/logo.svg" alt='logo' width={50} height={50} />
+        </Link>
+        <ul className='flex flex-row gap-5 items-center'>
+            <li>
+                <Link href={"/library"} className={cn(`text-base cursor-pointer capitalize`, pathName === "/library" ? "text-light-200" : "text-light-100")}>Library</Link>
+            </li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
