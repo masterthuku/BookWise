3fdc1880c4f61892e4309cbbf4003251cd0a5879
masterthuku <peterthuku706@gmail.com>
2025-01-19 22:47:25 +0300

    hello

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,27 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { Users } from "@/database/schema";
 
 import React from "react";
 
-const Home = () => {
+
+
+const Home = async () => {
+
+  const result = await db.select().from(Users)
+  console.log(JSON.stringify(result, null, 2));
+
   return (
     <>
+
+    {/*@ts-expect-error Server Component */}
+
       <BookOverview {...sampleBooks[0]} />
+
+      {/*@ts-expect-error Server Component */}
+      
       <BookList title="Latest Books" books={sampleBooks} className="mt-28" />
     </>
   );
